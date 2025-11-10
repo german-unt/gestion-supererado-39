8d5fb2a02875420b459305aec506595a2857f1bd
german-unt <german.mutuverria2@gmail.com>
2025-11-10 17:54:46 -0300

    add proveedor.h

--- gestionSupermercado39/proveedor.h
+++ gestionSupermercado39/proveedor.h
@@ -2,6 +2,23 @@
 #define PROVEEDOR_H_INCLUDED
 class proveedor{
 private:
+int idProveedor;
+char nombre [20];
+bool eliminado;
+
+public
+proveedor (int idproveedor, const char *nombre, bool eliminado);
+proveedor ();
+
+//setters
+void setIdProveedor (int s_idProveedor);
+void setNombre (const char *s_nombre);
+void setEliminado (bool s_eliminado);
+
+//getters
+int getIdProveedor();
+const char *getNombre();
+bool getEliminado();
 
 
 };
