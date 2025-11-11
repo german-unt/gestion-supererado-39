649a2cec64d16f275b68ab592f6202bb5a9c5e01
german-unt <german.mutuverria2@gmail.com>
2025-11-11 20:56:18 -0300

    add funciones globales

---
+++ gestionSupermercado39/funcionesGlobales.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include <iostream>
+#include "funcionesGlobales.h"
+
+void cargarCadena(char *palabra, int tamano)
+ {
+    int i=0;
+    fflush(stdin);
+    for (i=0; i<tamano; i++)
+   {
+     palabra[i]=cin.get();
+     if (palabra[i]=='\n')
+     {
+        break;
+     }
+   }
+    palabra[i]='\0';
+    fflush(stdin);
+ }
---
+++ gestionSupermercado39/funcionesGlobales.h
@@ -0,0 +1,8 @@
+#ifndef FUNCIONESGLOBALES_H_INCLUDED
+#define FUNCIONESGLOBALES_H_INCLUDED
+using namespace std;
+
+///Funciones
+void cargarCadena(char *palabra, int tamano);
+
+#endif // FUNCIONESGLOBALES_H_INCLUDED
